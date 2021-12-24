bf2a2bae25cab7ffb237a15d9b9904cf01e9a420
echokilo515 <echokilo515@gmail.com>
2021-12-24 08:40:36 -1000

    Updates

---
+++ precache-manifest.74324faf60a736cf701c4bc89b478917.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f7a470e1d6c89626574",
+    "url": "/monsters-rolodex/static/css/main.48c109b4.chunk.css"
+  },
+  {
+    "revision": "6f7a470e1d6c89626574",
+    "url": "/monsters-rolodex/static/js/main.6f7a470e.chunk.js"
+  },
+  {
+    "revision": "8ee15efa1a2054ecf258",
+    "url": "/monsters-rolodex/static/js/1.8ee15efa.chunk.js"
+  },
+  {
+    "revision": "4933064c23f62bd41ef2",
+    "url": "/monsters-rolodex/static/js/2.4933064c.chunk.js"
+  },
+  {
+    "revision": "84e3ea8691145683fd26",
+    "url": "/monsters-rolodex/static/js/runtime~main.84e3ea86.js"
+  },
+  {
+    "revision": "0060a8f5a44532e6b1a65750027ad6c0",
+    "url": "/monsters-rolodex/index.html"
+  }
+];
